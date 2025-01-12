fb91edc34fb9eeec653fbd49b9b6eb42613c61e8
KovalevskaOlga <olgggavolnaja@gmail.com>
2025-01-12 15:08:09 +0200

    reworked task 4

--- hw15.js
+++ hw15.js
@@ -1,6 +1,6 @@
 const people = [
-    { name: "Ola", country: "Ukraine", balance: 177,  friends: ['Tima', 'Oksana'], skills: ['Painting', 'Cooking'] },
-    { name: "Bob", country: "Germany", balance: 240,  friends: ['Snail'], skills: ['Guitar', 'Cycling']  },
+    { name: "Ola", country: "Ukraine", balance: 177,  friends: ['Tima', 'Oksana'], skills: ['Fishing', 'Cooking'] },
+    { name: "Bob", country: "Germany", balance: 240,  friends: ['Snail'], skills: ['Fishing', 'Cycling']  },
     { name: "John", country: "Italy", balance: 679,  friends: ['Olha', 'Olena', 'Barys', 'Ihor'], skills: ['Photography', 'Dancing']   },
     { name: "Ivan", country: "Poland", balance: 43, friends: ['Mary', 'BoB', 'Tima'], skills: ['Carpentry', 'Fishing']  },
 ];
@@ -9,13 +9,12 @@ const people = [
 const countBalance = people.reduce((acc,person) => acc + person.balance, 0)
 console.log(countBalance); 
 
-// task 2 Масив імен всіх користувачів у яких є друг із зазначеним ім'ям.
-const findTima = people.filter((person) => person.
-    friends.includes('Tima')).map((person) => person.name); 
-console.log(findTima)
+// // task 2 Масив імен всіх користувачів у яких є друг із зазначеним ім'ям.
+// const copyPeople = [...people]; // копія масиву тому що сорт мутує 
+// const findFriend = copyPeople.sort(())
 
 // task 3 Масив імен (поле name) людей, відсортованих в залежності від кількості їх друзів (поле friends)
-const copyPeople = [...people]; // копія масиву, тому що сорт мутує масив
+// використала копію масиву з попередньої таски
 const sortfriends = copyPeople.sort((prevFriend,
     nextFriend) => prevFriend.friends.length - nextFriend.
     friends.length).map((person) => person.name)
@@ -23,14 +22,10 @@ console.log(sortfriends);
 
 // task 4 Отримати масив всіх умінь всіх користувачів (поле skills), при цьому не має бути повторюваних умінь і вони повинні бути відсортовані в алфавітному порядку.
 let togetherSkills = [];
-const findSkills = people.forEach((person) => togetherSkills.push(...person.skills));
+const findSkills = people.forEach((person) => togetherSkills.push(...person.skills)); // об'єднала усі скіли в один масив
+// console.log(togetherSkills)
+const uniqFindSkills = togetherSkills.reduce((uniq, skill) => {
+  return uniq.includes(skill) ? uniq : [...uniq, skill]; // скоротила if else
+}, [])
+console.log(uniqFindSkills);
 
-let uniqueSkills = [];
-togetherSkills.forEach((skill) => {
-  if (!uniqueSkills.includes(skill)) {
-    uniqueSkills.push(skill);
-  }
-});
-
-uniqueSkills.sort();
-console.log(uniqueSkills);
